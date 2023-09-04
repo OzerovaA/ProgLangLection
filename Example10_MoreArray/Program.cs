242969d5954d8b567d2a12efbd0f4acfbc7314b9
OzerovaA <ozerova.anastasia.v@gmail.com>
2023-09-04 19:11:00 +0300

    Добавила пример 10

---
+++ Example10_MoreArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {16,22,33,42,15,68,97,81};
+int n = array.Length;
+Console.Write("Введите число для поиска: ");
+int find = int.Parse(Console.ReadLine()!);
+int index=0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
