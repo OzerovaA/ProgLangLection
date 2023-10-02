c103dba0e1c5b32cf9f99e9a7b0b832367f3d104
OzerovaA <ozerova.anastasia.v@gmail.com>
2023-10-02 16:31:25 +0300

    Добавила вторую версию для 4 типа методов в пример 12 (с циклом for)

--- Lec3Example012Method/Program.cs
+++ Lec3Example012Method/Program.cs
@@ -33,14 +33,24 @@ int Method3()
 
 //Вид 4, что-то принимают и что-то возвращают
 
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = string.Empty;
+//     while (i<count)
+//     {
+//         result=result+text;
+//         i++;
+//     }
+//     return result;
+// }
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = string.Empty;
-    while (i<count)
+    for(int i = 0; i<count; i++)
     {
         result=result+text;
-        i++;
     }
     return result;
 }
