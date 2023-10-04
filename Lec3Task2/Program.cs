6cf201eae64f9ba8872c824b5c416021d43d50dd
OzerovaA <ozerova.anastasia.v@gmail.com>
2023-10-04 13:31:52 +0300

    Добавлена часть задания 2 лекции 3

--- Lec3Task2/Program.cs
+++ Lec3Task2/Program.cs
@@ -1,1 +1,30 @@
-﻿// 
+﻿// Сортировка массива
+
+int[] arr = {1,5,4,3,2,6,7,1,1};
+
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+
+    for(int i =0 ; i<count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+void SelestionSort(int[] array)
+{
+     for(int i =0 ; i<array.Length; i++)
+     {
+        int minPosition = i;
+         for(int i =0 ; i<array.Length; i++)
+         {
+            //Лекция 3 , 35:24
+         }
+        int temporary = array [i];
+        array [i] = array [minPosition];
+        array [minPosition] = temporary;
+     }
+}
+
+PrintArray(arr);
